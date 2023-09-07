0ddeb616f71051bbe6234c461f157c203efa3617
FredMbugua <fredmbugua004@gmail.com>
2023-09-07 06:14:46 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  *
- * Return: 1 (Successful)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
