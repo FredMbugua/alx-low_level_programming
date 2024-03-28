a1b3484a02b4aa7707089e7d0ca7073e00142af7
FredMbugua <fredmbugua004@gmail.com>
2024-03-28 00:16:21 -0700

    Write system call

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,11 @@
  *
  * Return: 1 (Success)
  */
+
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout";
+	write(STDERR_FILENO, message, sizeof(message));
+	return (1);
 }
+
