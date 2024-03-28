6515ab6b2f4f0e5c641c7efe11690387116aa31c
FredMbugua <fredmbugua004@gmail.com>
2024-03-28 01:46:34 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	const char message[] = "and that piece of art is useful\" - \
-Dora Korpar, 2015-10-19";
+Dora Korpar, 2015-10-19\n";
 
 	write(STDOUT_FILENO, message, sizeof(message));
 
