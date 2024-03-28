6259e6067274a7b4bfc0f4ae3b638bf0ace72c5f
FredMbugua <fredmbugua004@gmail.com>
2024-03-28 00:53:42 -0700

    Puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,7 @@
 
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout";
-	write(STDERR_FILENO, message, sizeof(message));
+	puts="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout";
 	return (1);
 }
 
