8e76fe02ed713858ae89ad773f1e4a0460ec2752
FredMbugua <fredmbugua004@gmail.com>
2024-03-28 01:17:25 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -8,7 +9,11 @@
 
 int main(void)
 {
-	puts="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout";
+	const char message[] = "and that piece of art is useful\" -"
+	       	" Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, message, sizeof(message));
+
 	return (1);
 }
 
