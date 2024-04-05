72d3a1131e4e8006e9386c214b09fa5843703d7f
FredMbugua <fredmbugua004@gmail.com>
2024-04-05 15:30:25 -0700

    Sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,18 @@
 
  /**
   * add - adds two integers and returns the result
+  *
   * @a: int to be added to b
   * @b: int to be added to a
+  *
   * Return: Sum of a and b
   */
 int add(int a, int b)
 {
-int c;
+	int c;
 
-c = a + b;
+	c = a + b;
 
-return (c);
+	return (c);
 }
+
