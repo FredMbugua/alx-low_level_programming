8e2d60a2cd9bee02f900ae87b1f4b8ce1f2e4cdd
FredMbugua <fredmbugua004@gmail.com>
2024-04-05 13:54:30 -0700

    Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,10 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
+ *
  * starting from 00:00 to 23:59
  */
+
 void jack_bauer(void)
 {
 	int i, j;
@@ -26,3 +28,4 @@ void jack_bauer(void)
 		i++;
 	}
 }
+
