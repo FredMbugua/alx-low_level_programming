1f15487fd1ec1c4151002ac0539209fbcf6a8c27
FredMbugua <fredmbugua004@gmail.com>
2024-04-06 15:39:46 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,34 +16,26 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
+				if (j == 0){
+					_putchar(k + '0');}
+				else if (k < 10 && j != 0){
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k > 9 && k < 100)
-				{
+					_putchar(k + '0');}
+				else if (k > 9 && k < 100){
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k > 99)
-				{
+					_putchar((k % 10) + '0');}
+				else if (k > 99){
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
-					_putchar(((k % 10) % 10) + '0');
-				}
+					_putchar(((k % 10) % 10) + '0');}
 			}
 			_putchar('\n');
 		}
