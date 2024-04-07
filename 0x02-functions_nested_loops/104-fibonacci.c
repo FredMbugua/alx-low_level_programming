428379f597b300cfa7c62659bc73dd7482af060a
FredMbugua <fredmbugua004@gmail.com>
2024-04-07 00:18:05 -0700

    Fibonacci4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,13 +11,13 @@ int main(void)
 	long double fib1 = 1, fib2 = 2, next;
 	int count;
 
-	printf("%.0lf, %.0lf", fib1, fib2);
+	printf("%.0Lf, %.0Lf", fib1, fib2);
 
 	for (count = 3; count <= 98; count++)
 	{
 	next = fib1 + fib2;
 
-	printf(", %.0lf", next);
+	printf(", %.0Lf", next);
 
 	fib1 = fib2;
 	fib2 = next;
