d2bebb57fbfa93bbe278d414c58fda5553d27839
FredMbugua <fredmbugua004@gmail.com>
2024-04-11 13:14:24 -0700

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,9 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
+ *
  * @n: input
+ *
  * Return: n
  */
 
@@ -11,3 +13,4 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
