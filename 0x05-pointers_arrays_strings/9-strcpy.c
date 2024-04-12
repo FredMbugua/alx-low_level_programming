32c8c92a5c82b3247f2c604a8bd26c4fd935f124
FredMbugua <fredmbugua004@gmail.com>
2024-04-12 13:34:23 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,8 +2,10 @@
 
 /**
  * char *_strcpy - a function that copies the string pointed to by src
+ *
  * @dest: copy to
  * @src: copy from
+ *
  * Return: string
  */
 char *_strcpy(char *dest, char *src)
@@ -22,3 +24,4 @@ char *_strcpy(char *dest, char *src)
 	dest[l] = '\0';
 	return (dest);
 }
+
