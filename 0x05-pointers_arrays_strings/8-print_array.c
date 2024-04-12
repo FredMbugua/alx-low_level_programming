72bc04ab60322dc98a89dc53d159502ae98e1649
FredMbugua <fredmbugua004@gmail.com>
2024-04-12 13:29:35 -0700

    printarray

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,8 +2,10 @@
 
 /**
  * print_array - a function that prints n elements of an array
+ *
  * @a: array name
  * @n: is the number of elements OF the array to be printed
+ *
  * Return: a and n inputs
  */
 
@@ -21,3 +23,4 @@ void print_array(int *a, int n)
 		}
 			printf("\n");
 }
+
