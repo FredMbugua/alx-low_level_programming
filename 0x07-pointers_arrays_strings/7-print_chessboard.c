00abab726e5aa8021eccb98013bcd9f16122c4da
FredMbugua <fredmbugua004@gmail.com>
2024-04-19 06:23:00 -0700

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,9 @@
 
 /**
  * print_chessboard - Entry point
+ *
  * @a: array
+ *
  * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
@@ -17,3 +19,4 @@ void print_chessboard(char (*a)[8])
 		_putchar('\n');
 	}
 }
+
