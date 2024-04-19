a3d3b4baa338ba3a96f2c19aa964137d833ebcc6
FredMbugua <fredmbugua004@gmail.com>
2024-04-19 03:34:18 -0700

    strbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,14 @@
 #include "main.h"
+
 /**
  * _strpbrk - Entry point
+ *
  * @s: input
  * @accept: input
+ *
  * Return: Always 0 (Success)
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 		int k;
@@ -21,3 +25,4 @@ char *_strpbrk(char *s, char *accept)
 
 	return ('\0');
 }
+
