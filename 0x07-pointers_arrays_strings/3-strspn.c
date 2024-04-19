f6ed0af8cb5c9386d5845afbad75c623d4d0f3f0
FredMbugua <fredmbugua004@gmail.com>
2024-04-19 02:52:10 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,10 +2,14 @@
 
 /**
  * _strspn - Entry point
+ *
  * @s: input
+ *
  * @accept: input
+ *
  * Return: Always 0 (Success)
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
