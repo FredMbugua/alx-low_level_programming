f7280252dda8c789312829c89afbfa716b55467f
FredMbugua <fredmbugua004@gmail.com>
2024-04-28 15:21:24 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,46 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+int is_valid_number(const char *str);
+int multiply(int num1, int num2);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        fprintf(stderr, "Error\n");
+        return 98;
+    }
+
+    if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]))
+    {
+        fprintf(stderr, "Error\n");
+        return 98;
+    }
+
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+
+    int result = multiply(num1, num2);
+    printf("%d\n", result);
+
+    return 0;
+}
+
+int is_valid_number(const char *str)
+{
+    while (*str)
+    {
+        if (*str < '0' || *str > '9')
+        {
+            return 0;
+	}
+	str++;
+	}
+	return 1;
+}
+
+int multiply(int num1, int num2)
+{
+	return num1 * num2;
+}
